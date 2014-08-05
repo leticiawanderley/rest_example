9fa2df64dc555142a6229fedcdc33691abcbff67
João Pedro Leôncio <joopeeds@gmail.com>
2014-08-04 21:28:01 -0300

    Post handlers

--- main.py
+++ main.py
@@ -14,12 +14,46 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-import webapp2
+import webapp2, re
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         self.response.write('Hello world!')
 
+class PostCollectionHandler(webapp2.RequestHandler):
+
+    def get(self):
+        # 200 (OK), list of Posts. JSON content-type
+        pass
+
+    def head(self):
+        # 200 (OK), JSON content-type
+        pass
+
+    def post(self):
+        #201 (Created), 'Location' header with link to /post/:id containing new ID.
+        pass
+
+
+class PostIndividualHandler(webapp2.RequestHandler):
+    def get(self, id):
+        #200 (OK), the post. 404 (Not Found), if ID not found or invalid. JSON content-type
+        pass
+
+    def head(self, id):
+        #200 (OK). 404 (Not Found), if ID not found or invalid. JSON content-type
+        pass
+
+    def put(self, id):
+        #204 (No Content). 404 (Not Found), if ID not found or invalid.
+        pass
+
+    def delete(self, id):
+        #200 (OK). 404 (Not Found), if ID not found or invalid.
+        pass
+
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainHandler),
+    ('/post', PostCollectionHandler),
+    (r'/post/(\d+)', PostIndividualHandler)
 ], debug=True)
